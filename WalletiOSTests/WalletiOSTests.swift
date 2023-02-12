91d1b9bc7cab4b8da3d731681a002732c2cfe15e
Tomek Kuzma <kuzma.tomek@gmail.com>
2023-02-12 18:34:07 +0100

    fixed unit test compilation

---
+++ WalletiOSTests/WalletiOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  WalletiOSTests.swift
+//  WalletiOSTests
+//
+//  Created by Tomek Kuzma on 12/02/2023.
+//
+
+import XCTest
+import WalletCore
+
+final class WalletiOSTests: XCTestCase {
+
+    func testExample() throws {
+        // logic tested in the WalletCore
+    }
+}
